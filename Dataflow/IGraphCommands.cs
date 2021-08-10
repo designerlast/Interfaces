aa0fa9cbee8758b8a8d2b1d1df30a4de7d3b84ea
Rio <4218831@qq.com>
2021-08-10 11:47:21 +0800

    Add IGraaphCommand. Change dependency from NetCore3.1 to NetStandard2.1.

---
+++ Dataflow/IGraphCommands.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces
+{
+    public interface IGraphCommands
+    {
+        void OnOpenCommandExcute();
+        void OnSaveCommandExcute();
+        void OnRunCommandExcute();
+    }
+}
