da7009c837a4d1950755f01331b17572da6bc365
Rio <4218831@qq.com>
2021-09-26 18:15:27 +0800

    Added IImageListBatchRun

---
+++ IImageListBatchRun.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces
+{
+    public interface IImageListBatchRun
+    {
+        public event Action ResetImageSequence;
+        public event Func<bool> MoveToNextImage;
+    }
+}
