9cb5278fdc4d1d76f1f791bc7462f6cb84103c02
王正 <wangzheng@megarobo.tech>
2021-10-29 13:25:45 +0800

    Add some interfaces

---
+++ IAddToLabelList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces
+{
+    public interface IAddToLabelList
+    {
+        void AddToLabelList<T>(T obj);
+    }
+}
---
+++ IEditImage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces
+{
+    public interface IEditImage
+    {
+        void EditImage<T>(T obj);
+    }
+}
