e19123aad71e53f0001f74ae39bfde84e6107958
李旭光 <lixuguang@megarobo.tech>
2021-12-27 09:57:43 +0800

    feat: 新增写日志接口类；

---
+++ IWriteLog.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces
+{
+    public interface IWriteLog
+    {
+        void WriteLog(EventId eventId,string messType, string message);
+    }
+}
