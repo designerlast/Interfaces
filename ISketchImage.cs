695a9e6cd52bd577df41eda8de7def655f8e72ba
王正 <wangzheng@megarobo.tech>
2021-12-30 16:37:28 +0800

    feat: add ISketchImage, used for SketchImage camera status.

---
+++ ISketchImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces
+{
+    public interface ISketchImage
+    {
+        /// <summary>
+        /// 设置SketchImage顶部状态栏的相机连接状态和颜色
+        /// </summary>
+        /// <param name="statusText"></param>
+        /// <param name="statusTextHexColor"></param>
+        public void SetCameraStatus(string statusText, string statusTextHexColor);
+    }
+}
