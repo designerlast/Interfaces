5d1d201b3af7989c6b95f770c52257e5d8de193c
李旭光 <lixuguang@megarobo.tech>
2022-01-04 15:51:02 +0800

    feat: 添加相机和Socket通信接口；

---
+++ Camera/VegaImage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces.Camera
+{
+    public class VegaImage
+    {
+        public VegaFrameInfo FrameInfo { get; set; }
+        public byte[] OriginalDataArray { get; set; }
+    }
+}
---
+++ Camera/VegaPixelFormat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces.Camera
+{
+    /// <summary>
+    /// 像素格式枚举，目前照抄海康的作为标准。
+    /// </summary>
+    public enum VegaPixelFormat
+    {
+        Mono8 = 17301505,
+        Mono10 = 17825795,
+        Mono10Packed = 17563652,
+        Mono12 = 17825797,
+        Mono12Packed = 17563654,
+        Mono16 = 17825799,
+        RGB8Packed = 35127316,// 0x02180014
+        YUV422_8 = 34603058,
+        YUV422_8_UYVY = 34603039,
+        BayerGR8 = 17301512,
+        BayerRG8 = 17301513,
+        BayerGB8 = 17301514,
+        BayerBG8 = 1217301515,
+        BayerGB10 = 17825806,
+        BayerGB12 = 17825810,
+        BayerGB12Packed = 17563692
+    }
+}
