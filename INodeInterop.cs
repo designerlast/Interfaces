dbbfefd2d15ac682a80e8a4ad204629a85d6b3b3
王正 <wangzheng@megarobo.tech>
2022-01-04 20:20:34 +0800

    feat: added INodeInterop

---
+++ INodeInterop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces
+{
+    public interface INodeInterop
+    {
+        object Terminals { get; }
+        IDelegate InnerDelegate { get; }
+    }
+}
