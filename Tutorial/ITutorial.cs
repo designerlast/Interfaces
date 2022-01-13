91c278a1088b8dd83bbd60287838d7475e088295
王正 <wangzheng@megarobo.tech>
2022-01-13 19:27:27 +0800

    feat: add ITutorial interface for quick start.

---
+++ Tutorial/ITutorial.cs
@@ -0,0 +1,12 @@
+﻿namespace IntellVega.CBB.Interfaces.Tutorial
+{
+    /// <summary>
+    /// This is a tutorial interface, do not use it in production!!!
+    /// This is a tutorial interface, do not use it in production!!!
+    /// This is a tutorial interface, do not use it in production!!!
+    /// </summary>
+    public interface ITutorial
+    {
+        void ChangeTutorialText(string text);
+    }
+}
