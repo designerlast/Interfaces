5516cc49100f04a6d0e3524f0b2872695f4a8fe7
李旭光 <lixuguang@megarobo.tech>
2022-03-01 14:45:15 +0800

    feat: 新增设置SketchImage顶部状态栏消息内容接口；

--- ISketchImage.cs
+++ ISketchImage.cs
@@ -5,11 +5,10 @@ namespace IntellVega.CBB.Interfaces
     public interface ISketchImage
     {
         /// <summary>
-        /// 设置SketchImage顶部状态栏的相机连接状态和颜色
+        /// 设置SketchImage顶部状态栏消息内容
         /// </summary>
-        /// <param name="statusText"></param>
-        /// <param name="statusTextHexColor"></param>
-        void SetCameraStatus(string statusText, string statusTextHexColor);
+        /// <param name="message"></param>
+        void SetTitleMessage(string message);
 
         /// <summary>
         /// 设置控件背景图片
