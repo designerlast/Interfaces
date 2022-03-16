5557a8c0582deabd47afb010fbc7f2486e800564
Vincent Wang <wangzheng@megarobo.tech>
2022-03-16 14:03:28 +0800

    feat: add ICamera interface.

---
+++ Camera/ICameraDeviceFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace IntellVega.CBB.Interfaces.Camera
+{
+    public interface ICameraDeviceFactory
+    {
+        string Name { get; }
+        IEnumerable<ICameraDevice> CreateAllDrivers();
+    }
+}
---
+++ Camera/ICameraService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces.Camera
+{
+    public interface ICameraService
+    {
+        IEnumerable<string> GetAllCameraNames();
+        ICameraDevice GetCameraByName(string name);
+
+    }
+}
