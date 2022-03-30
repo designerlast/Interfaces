107a659352f37bbeab0b65b7adf21465586939d3
李旭光 <lixuguang@megarobo.tech>
2022-03-30 20:55:51 +0800

    feat: 版本号更新1.7.62128；

--- Dataflow/IApplicationDataflow.cs
+++ Dataflow/IApplicationDataflow.cs
@@ -6,6 +6,12 @@ namespace IntellVega.CBB.Interfaces.Dataflow
     public interface IApplicationDataflow
     {
         /// <summary>
+        /// 流程新增元素
+        /// </summary>
+        /// <param name="item"></param>
+        void AddSourceItem(SourceItem item);
+
+        /// <summary>
         /// 获取流程图元素集合（子函数、变量、事件）
         /// </summary>
         /// <returns></returns>
