8f2ee4da86ad270aeff2ead08ca2e5ebf03c1768
李旭光 <lixuguang@megarobo.tech>
2022-04-08 17:10:29 +0800

    feat: 新增操作标注区接口类；

---
+++ DeepLearning/IOperationAIDrawing.cs
@@ -0,0 +1,9 @@
+﻿namespace IntellVega.CBB.Interfaces.DeepLearning
+{
+    public interface IOperationAIDrawing
+    {
+        void SetShapeSelected(string shapeID);
+
+        void DeleteDrawingShape(string shapeID);
+    }
+}
--- DeepLearning/ISetShapeSelected.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace IntellVega.CBB.Interfaces.DeepLearning
-{
-    public interface ISetShapeSelected
-    {
-        void SetShapeSelected(string id);
-    }
-}
