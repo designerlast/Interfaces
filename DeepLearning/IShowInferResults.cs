9e54c35ba6f0a95733a667adf234da27ff1c8c69
李旭光 <lixuguang@megarobo.tech>
2022-04-12 20:47:12 +0800

    feat: 修改推理结果统计接口；

--- DeepLearning/IShowInferResults.cs
+++ DeepLearning/IShowInferResults.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Text;
-
-namespace IntellVega.CBB.Interfaces.DeepLearning
+﻿namespace IntellVega.CBB.Interfaces.DeepLearning
 {
     public interface IShowInferResults
     {
-        void ShowInferResults(DataTable obj);
-        void ShowStatisticsInfo(int totalTime, int totalCount);
+        void ShowStatisticsInfo(int totalTime, int totalCount, double ar, double ap, double miou);
     }
 }
