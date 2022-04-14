c76857062b4a85d3092792138ffa698a78614593
李旭光 <lixuguang@megarobo.tech>
2022-04-14 14:43:08 +0800

    feat: 接口类整理，删除无用文件；发布1.8.4.2142；

---
+++ DeepLearning/IOperationImageList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace IntellVega.CBB.Interfaces.DeepLearning
+{
+    public interface IOperationImageList
+    {
+        /// <summary>
+        /// 设置选中图片
+        /// </summary>
+        /// <param name="imagePath">当前图片路径</param>
+        /// <param name="isNext">是否下一张 true:是 false：上一张</param>
+        void SetSelectedImage(string imagePath, bool isNext);
+
+        /// <summary>
+        /// 设置图片标注状态
+        /// </summary>
+        /// <param name="imagePath">图片路径</param>
+        /// <param name="isMarked">True: 已标注 False: 未标注</param>
+        void SetImageMarkState(string imagePath,bool isMarked);
+
+        /// <summary>
+        /// 获取推理图片集
+        /// </summary>
+        /// <returns></returns>
+        List<string> GetImagesForReasoning();
+
+        /// <summary>
+        /// 获取训练图片集
+        /// </summary>
+        /// <returns></returns>
+        List<string> GetImagesForTraining();
+    }
+}
