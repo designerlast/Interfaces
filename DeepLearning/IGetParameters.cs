9a692595906c54b9e583af4e50432add51f217fd
李旭光 <lixuguang@megarobo.tech>
2022-04-16 11:00:54 +0800

    feat: 新增训练参数、推理参数获取接口；

---
+++ DeepLearning/IGetParameters.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace IntellVega.CBB.Interfaces.DeepLearning
+{
+    public interface IGetParameters
+    {
+        /// <summary>
+        /// 获取训练参数
+        /// </summary>
+        /// <returns></returns>
+        Dictionary<string, object> GetTrainParameters();
+
+        /// <summary>
+        /// 获取推理参数
+        /// </summary>
+        /// <returns></returns>
+        Dictionary<string, object> GetReasonParameters();
+    }
+}
