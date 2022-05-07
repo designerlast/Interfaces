e38739b0efd52a895d0bf756e6b9d90bb1e4ad3f
hemeng <hemeng@megarobo.tech>
2022-05-07 13:44:02 +0800

    feat: vision project add interface

---
+++ Vision/IProductParameter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces.Vision
+{
+    public interface IProductParameter
+    {
+        /// <summary>
+        /// 更新产品名称
+        /// </summary>
+        /// <param name="productName">传入的产品名称</param>
+        void UpdateProductName(string productName);
+
+        /// <summary>
+        /// 更新UserControl的isEnabled状态
+        /// </summary>
+        /// <param name="isEnabled">传入的产品名称</param>
+        void UpdateIsEnabledStatus(bool isEnabled);
+    }
+}
