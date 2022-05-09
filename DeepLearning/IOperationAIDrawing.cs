ea25c3cf5aa1f3afd5cb1935364d2bf967cb96fc
李旭光 <lixuguang@megarobo.tech>
2022-05-09 10:18:50 +0800

    feat: 接口类整合；新增图像列表清空接口；

--- DeepLearning/IOperationAIDrawing.cs
+++ DeepLearning/IOperationAIDrawing.cs
@@ -2,6 +2,10 @@
 {
     public interface IOperationAIDrawing
     {
+        void EditImage(string imgPath);
+
+        void ClearImage();
+
         void SetShapeSelected(string shapeID);
 
         void DeleteDrawingShape(string shapeID);
