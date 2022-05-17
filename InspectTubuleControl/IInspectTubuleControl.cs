fa7103bdb017d75d0f8d73f828ecb06a7077301c
zhoujing <zhoujing@megarobo.tech>
2022-05-17 10:34:33 +0800

    feat: 配方接口增加bitmap参数

--- InspectTubuleControl/IInspectTubuleControl.cs
+++ InspectTubuleControl/IInspectTubuleControl.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace IntellVega.CBB.Interfaces.InspectTubuleControl
 {
     public interface IInspectTubuleControl
     {
-        void RunRecipe(RecipeModel recipeModel);
+        void RunRecipe(RecipeModel recipeModel, Bitmap bitmap);
         void SetChildViewModel(RecipeEnum imageSource, object viewModel);
     }
 }
