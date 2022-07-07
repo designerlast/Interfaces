fe0940be6f65fa33f6a05299f0bd297e0f980501
李旭光 <lixuguang@megarobo.tech>
2022-07-07 11:03:03 +0800

    feat: 新增IMessageBox接口类；

---
+++ IMessageBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces
+{
+    public interface IMessageBox
+    {
+        void Info(string message);
+
+        void Success(string message);
+
+        void Warning(string message);
+
+        void Error(string message);
+
+        bool Ask(string message);
+    }
+}
