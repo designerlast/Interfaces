2d8fcea56782abe74496cb3bf2968f8b3aa4f541
李旭光 <lixuguang@megarobo.tech>
2022-07-11 17:42:40 +0800

    feat: 新增SelectColorAttribute类；版本号修改；

---
+++ Attributes/SelectColorAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace IntellVega.CBB.Interfaces.Attributes
+{
+    public class SelectColorAttribute : RemarkAttribute
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="remarkLocalName">提示文字</param>
+        /// <param name="isEnabled">是否可用</param>
+        public SelectColorAttribute(string remarkLocalName, bool isEnabled = true) : base(remarkLocalName, isEnabled)
+        {
+
+        }
+    }
+}
