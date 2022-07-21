595c269dd0c3e5303b2baa0672f062dcd676a9ae
李旭光 <lixuguang@megarobo.tech>
2022-07-21 20:24:23 +0800

    feat: 新增运行状态接口类及枚举；

---
+++ RuningState/IRuningState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces.RuningState
+{
+    public interface IRuningState
+    {
+        /// <summary>
+        /// 通知程序运行状态
+        /// </summary>
+        /// <param name="stateType">停止中：Stopping = 0 运行中：Runing = 1</param>
+        void NotifyRuningState(StateType stateType);
+    }
+}
---
+++ RuningState/StateType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces.RuningState
+{
+    public enum StateType
+    {
+        [Display(Name = "停止中")]
+        Stopping = 0,
+
+        [Display(Name = "运行中")]
+        Runing = 1,
+
+        [Display(Name = "暂停")]
+        Suspend =2,
+
+        [Display(Name = "错误")]
+        Error = 3,
+    }
+}
