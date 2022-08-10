eb28aaee70778f01af8cbfb0c8e1caf31af968d8
zhoujing <zhoujing@megarobo.tech>
2022-08-10 14:24:17 +0800

    feat: 设备状态页面

---
+++ DevicesStatus/IDevicesStatus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces.DevicesStatus
+{
+    public interface IDevicesStatus
+    {
+        /// <summary>
+        /// 添加设备
+        /// </summary>
+        /// <param name="deviceName"></param>
+        void AddDevice(string deviceName);
+        /// <summary>
+        /// 移除设备
+        /// </summary>
+        /// <param name="deviceName"></param>
+        void RemoveDevice(string deviceName);
+        /// <summary>
+        /// 更新设备状态
+        /// </summary>
+        /// <param name="deviceName"></param>
+        /// <param name="isConnected"></param>
+        void UpdateDeviceStatus(string deviceName, bool isConnected);
+        /// <summary>
+        /// 清除设备状态列表
+        /// </summary>
+        void Clear();
+
+    }
+}
