36170992dcd486f741258556f0a819996de56297
zhoujing <zhoujing@megarobo.tech>
2022-08-24 16:34:29 +0800

    feat: 修改设备连接状态接口名称

---
+++ DevicesStatus/IConnectionStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces.DevicesStatus
+{
+    public interface IConnectionStatus
+    {
+        /// <summary>
+        /// 更新设备连接状态
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="isConnected"></param>
+        void UpdateDeviceStatus(string name, bool isConnected);
+
+    }
+}
