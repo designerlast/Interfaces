3c6ff56c7ce168b77d2e9fb5b63c6eb45996c652
zhoujing <zhoujing@megarobo.tech>
2022-09-14 09:26:38 +0800

    feat: 通讯接口

---
+++ Communication/ICommunicationDevice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces.Communication
+{
+    public interface ICommunicationDevice
+    {
+        /// <summary>
+        /// 通信实例名称
+        /// </summary>
+        public string DisplayName { get; }
+        public int Id { get; set; }
+        public void Open();
+        public void Close();
+        public void Send(string message);
+        public void Send(byte[] bytes);
+
+    }
+}
---
+++ Communication/ICommunicationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces.Communication
+{
+    public interface ICommunicationService
+    {
+        public IEnumerable<string> GetCommunicationDevicesNames();
+        public ICommunicationDevice GetCommunicationDeviceByName(string name);
+        public void AddCommunicationDevice(ICommunicationDevice communicationDevice);
+        public void RemoveCommunicationDevice(ICommunicationDevice communicationDevice);
+        public void CreateCommunicationDevices();
+    }
+}
