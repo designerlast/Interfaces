8cac17dfc7fdf420f3aa19746b4341176c147c2b
李旭光 <lixuguang@megarobo.tech>
2022-09-19 17:08:42 +0800

    feat: 全局Loding添加关闭功能及关闭回调事件；

--- MainFrame/IMainLoading.cs
+++ MainFrame/IMainLoading.cs
@@ -1,7 +1,11 @@
-﻿namespace IntellVega.CBB.Interfaces.MainFrame
+﻿using System;
+
+namespace IntellVega.CBB.Interfaces.MainFrame
 {
     public interface IMainLoading
     {
         void SetLoadingState(bool isShow);
+
+        event Action CloseMainLoadingAction;
     }
 }
