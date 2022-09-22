72c13c7a4917b356f33bf7b2157719c4818ccc90
wangzheng <wangzheng@megarobo.tech>
2022-09-22 14:54:31 +0800

    feat: 增加自定义异常类

---
+++ CustomException/MegaException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IntellVega.CBB.Interfaces.CustomException
+{
+    public class MegaException : Exception
+    {
+        public MegaException()
+        {
+
+        }
+        public MegaException(string message) : base(message)
+        {
+
+        }
+
+        public MegaException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
