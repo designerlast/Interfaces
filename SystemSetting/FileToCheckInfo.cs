fe2af3242ae4811400d94d73b3dfaa23afa5b180
wangzizheng <wangzizheng@megarobo.tech>
2022-09-26 10:28:38 +0800

    feat: 用于文件缺失，文件信息比对;

---
+++ SystemSetting/FileToCheckInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntellVega.CBB.Interfaces.SystemSetting
+{
+    public class FileToCheckInfo
+    {
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+    }
+}
