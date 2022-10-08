8dd393064bbc2822dd24a147c317c554c5597e79
zhoujing <zhoujing@megarobo.tech>
2022-10-08 18:26:41 +0800

    feat: 修改ROI管理器相关接口

--- ROIWindow/IROIWindowManager.cs
+++ ROIWindow/IROIWindowManager.cs
@@ -6,9 +6,9 @@ namespace IntellVega.CBB.Interfaces.ROIWindow
 {
     public interface IROIWindowManager
     {
-        public event EventHandler<List<object>> WindowClosed; 
-        public void Show();
-        public void ShowDialog();
-        public void Close();
+        public List<object> GetShapeList();
+        public bool ShowDialog();
+        public bool ShowAndSetVisual(object visual);
+        public void SetImageViewerVisualObject(object visualObject);
     }
 }
