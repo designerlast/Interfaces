b9b297e4b908e36986f38ae87f45d8001e7dd72a
李旭光 <lixuguang@megarobo.tech>
2022-10-31 15:54:08 +0800

    feat: 统计分析接口新增方法；发布2.3.24.2343版本；

--- Statistics/IStatistics.cs
+++ Statistics/IStatistics.cs
@@ -6,8 +6,28 @@ namespace IntellVega.CBB.Interfaces.Statistics
 {
     public interface IStatistics
     {
+        /// <summary>
+        /// 获取工位名称列表
+        /// </summary>
+        /// <returns></returns>
+        List<string> LoadStationNames();
+
+        /// <summary>
+        /// 加载统计数据
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
         List<StationStatistics> LoadStationStatistics(DateTime start, DateTime end);
 
+        /// <summary>
+        /// 获取查询报表数据
+        /// </summary>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
+        /// <param name="stations"></param>
+        /// <param name="checkResult"></param>
+        /// <returns></returns>
         List<RunRecord> SearchRecords(DateTime? startTime, DateTime? endTime, IEnumerable<string> stations, DetectionResult checkResult);
     }
 }
